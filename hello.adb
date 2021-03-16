022d4063b0375f9bc089a56910ae90c3fb5f8eb7
Julien Gueytat <contact@jgueytat.fr>
2021-03-16 19:39:17 +0000

    GDB output more clear

--- hello.adb
+++ hello.adb
@@ -2,5 +2,6 @@ with Ada.Text_IO;
 
 procedure Hello is
 begin
-   Ada.Text_IO.Put_Line ("Hello World !");
+   Ada.Text_IO.Put_Line ("Hello World Foo !");
+   Ada.Text_IO.Put_Line ("Hello World Bar !");
 end Hello;
