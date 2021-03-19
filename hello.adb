4e727f16819da17a51cf3246bb14fde11729e0e1
Julien Gueytat <contact@jgueytat.fr>
2021-03-19 14:28:14 +0000

    Align Ada project on C

--- hello.adb
+++ hello.adb
@@ -2,6 +2,6 @@ with Ada.Text_IO;
 
 procedure Hello is
 begin
-   Ada.Text_IO.Put_Line ("Hello World Foo !");
-   Ada.Text_IO.Put_Line ("Hello World Bar !");
+   Ada.Text_IO.Put_Line ("Hello Foo !");
+   Ada.Text_IO.Put_Line ("Hello Bar !");
 end Hello;
